18fe63e23aa07a1c2889b81b5117752b73fe8866
Igor Sinelnikoff <ingvarkneidler@gmail.com>
2024-05-22 01:49:47 +0500

    add executor

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from aiogram import types
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.types import ReplyKeyboardRemove
+from aiogram import executor
+from logging import basicConfig, INFO
 from data.config import ADMINS
-from loader import dp
+from loader import dp, db, bot
+
+
 
 
 user_message = 'Пользователь'
@@ -44,3 +48,12 @@ async def user_mode(message: types.Message):
 
     await message.answer('Включен пользовательский режим.',
                          reply_markup=ReplyKeyboardRemove())
+
+
+async def on_startup(dp):
+    basicConfig(level=INFO)
+    db.create_tables()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
